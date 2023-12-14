d4b30ec2960ba38838bc9047d7fc3849689f16d2
lealb2485 <144816112+lealb2485@users.noreply.github.com>
2023-12-14 11:02:52 -0500

    Add files via upload

---
+++ P1HW2_Leal,Benjamin.py
@@ -0,0 +1,24 @@
+# This program calculates and displays travel expenses
+# 9/5/23
+# CTI-110 P1HW2 - Travel Expense
+# Benjamin Leal
+#
+
+print ('This program calculates and diplays travel expenses. \n')
+budget = float(input("Enter budget:  "))
+print()
+destination = input("Enter trvel destination: \n")
+print()
+gas = float(input("How much do you think you will spend on gas?\n"))
+print()
+hotel = float(input("About how much do you thin you will spend on accomidations?\n"))
+print()
+Food = float(input("How much will you spend on food?\n"))
+print()
+
+print('-------------Travel Expenses-------------')
+print("location:",destination)
+print("initial budget:" ,budget)
+print("\nFuel:" ,gas)
+print("Accomidations:" , hotel)
+print("Food:" ,Food)
