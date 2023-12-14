83d41c2fd5afc640c0a39910aadd5502c44bcf00
lealb2485 <144816112+lealb2485@users.noreply.github.com>
2023-12-14 11:06:08 -0500

    Add files via upload

---
+++ P1HW1_Leal,Benjamin.py
@@ -0,0 +1,9 @@
+
+number = int(input('Enter integer:\n'))
+print ('You entered:', number)
+print (number,'squared is', number * number)
+print ('And' , number, 'cubed is', number * number * number,'!!')
+number2 = int(input('Enter another integer:\n'))
+answer = number + number2
+print (number, '+', number2,'is',number + number2)
+print (number, '*', number2,'is',number * number2)
